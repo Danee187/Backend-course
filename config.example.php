07faf42ad92aacbc5988d8aba0238c18eae6c345
kisdani <kisdani0720@gmail.com>
2019-03-27 13:53:28 +0100

    4 lesson: Refactor

---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'site' =>[
+        'title' => 'Random site vagyok!!!'
+    ],
+    'database' => [
+        'dsn' => 'mysql:dbname=myapp;host=127.0.0.1',
+        'username' => 'root',
+        'password' => ''
+    ]
+];
